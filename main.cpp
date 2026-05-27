01c859a6a2d82315ad2a34490481e98bdce02ac8
victorvichi <victor.vichi@edu.senai.br>
2026-05-27 13:59:02 -0300

    feat: salvamento em arquivo

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <algorithm>
 #include <windows.h>
+#include <fstream>
 
 using namespace std;
 
-int main() {
+int main()
+{
     SetConsoleOutputCP(65001);
 
     string nomes[20];
@@ -16,36 +18,41 @@ int main() {
 
     cout << "=== SISTEMA DE NOTAS v4.0 ===" << endl;
 
-    do {
+    do
+    {
         cout << "Quantidade de alunos (1 a 20): ";
         cin >> qntdAlunos;
     } while (qntdAlunos < 1 || qntdAlunos > 20);
 
     cin.ignore();
 
-    for (int i = 0; i < qntdAlunos; i++) {
+    for (int i = 0; i < qntdAlunos; i++)
+    {
         cout << "Nome do aluno " << i + 1 << ": ";
         getline(cin, nomes[i]);
     }
 
-    do {
+    do
+    {
         cout << "Quantidade de disciplinas (1 a 5): ";
         cin >> qntdDisciplinas;
     } while (qntdDisciplinas < 1 || qntdDisciplinas > 5);
 
-    
-    cin.ignore(); 
+    cin.ignore();
 
-    for (int i = 0; i < qntdAlunos; i++) {
+    for (int i = 0; i < qntdAlunos; i++)
+    {
         cout << "\nNotas do aluno: " << nomes[i] << endl;
         float soma = 0;
-        for (int j = 0; j < qntdDisciplinas; j++) {
-            do {
+        for (int j = 0; j < qntdDisciplinas; j++)
+        {
+            do
+            {
                 cout << "  Disciplina " << j + 1 << " (0 a 10): ";
                 cin >> notas[i][j];
-                
-            } while (notas[i][j] < 0 || notas[i][j] > 10); 
-            
+
+            } while (notas[i][j] < 0 || notas[i][j] > 10);
+
             soma += notas[i][j];
         }
         media[i] = soma / qntdDisciplinas;
@@ -53,27 +60,70 @@ int main() {
 
     cout << "\n==============================" << endl;
     cout << "RELATÓRIO FINAL:" << endl;
-    int alunosAprovados = 0; int alunosReprovados = 0; int alunosRecuperacao = 0;
+    int alunosAprovados = 0;
+    int alunosReprovados = 0;
+    int alunosRecuperacao = 0;
 
-    for (int i = 0; i < qntdAlunos; i++) {
+    for (int i = 0; i < qntdAlunos; i++)
+    {
         cout << nomes[i] << " - Média: " << media[i] << endl;
-        if (media[i] >= 7) {
+        if (media[i] >= 7)
+        {
             cout << "  Situação: Aprovado" << endl;
             alunosAprovados++;
-        } else if (media[i] >= 5) {
+        }
+        else if (media[i] >= 5)
+        {
             cout << "  Situação: Recuperação" << endl;
             alunosRecuperacao++;
-        } else {
+        }
+        else
+        {
             cout << "  Situação: Reprovado" << endl;
             alunosReprovados++;
         }
     }
     cout << "===============================" << endl;
-    cout << "resumo: " << endl;
-    cout << "alunos aprovados: " << alunosAprovados << endl;
-    cout << "alunos em recuperação: " << alunosRecuperacao << endl;
-    cout << "alunos reprovados: " << alunosReprovados << endl;
+    cout << "Resumo: " << endl;
+    cout << "Alunos aprovados: " << alunosAprovados << endl;
+    cout << "Alunos em recuperação: " << alunosRecuperacao << endl;
+    cout << "Alunos reprovados: " << alunosReprovados << endl;
     cout << "===============================" << endl;
 
+    // === CORREÇÃO APLICADA AQUI ABAIXO ===
+    ofstream arquivo("relatorio_notas.txt");
+    if (arquivo.is_open())
+    {
+        arquivo << "=== RELATÓRIO ===" << endl;
+        for (int i = 0; i < qntdAlunos; i++)
+        {
+            arquivo << nomes[i] << " -  Média: " << media[i] << endl;
+            if (media[i] >= 7)
+            {
+                arquivo << "Aprovado" << endl;
+            }
+            else if (media[i] >= 5) // O 'else if' agora está corretamente dentro do laço for
+            {
+                arquivo << "Recuperação" << endl;
+            }
+            else
+            {
+                arquivo << "Reprovado" << endl;
+            }
+        } 
+
+        arquivo << "\nResumo:" << endl;
+        arquivo << alunosAprovados << " alunos aprovados" << endl;
+        arquivo << alunosRecuperacao << " alunos em recuperação" << endl;
+        arquivo << alunosReprovados << " alunos reprovados" << endl;
+        
+        arquivo.close();
+        cout << "Relatório salvo em 'relatorio_notas.txt'" << endl;
+    }
+    else
+    {
+        cout << "Erro ao criar o arquivo" << endl;
+    }
+    
     return 0;
 }
